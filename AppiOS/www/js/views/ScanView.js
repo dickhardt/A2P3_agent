30f709ce9c0f9bbf6a91b1d8c78c2efd0191e5f9
Greg Turner <greg.fun@gmail.com>
2013-01-16 16:54:50 -0800

    move scan function to home view

--- AppiOS/www/js/views/ScanView.js
+++ AppiOS/www/js/views/ScanView.js
@@ -17,38 +17,6 @@ $(function($) {
 	
 		events: {
                                                  },
-	    
-	    /*
-	     * Event handler addScan(): 
-	     */
-	    scan: function() {
-	    	console.log("Begin scan");
-	        window.plugins.barcodeScanner.scan(
-                function(result) {
-                	console.log("Scan success callback");
-                    if (result.cancelled) {
-                        navigator.notification.alert("Scan Cancelled");
-                        app.navigate("home");
-                    }
-                    else {
-                    	// Two flavours, enroll QR and logon QR
-                    	// Simple logic, if it starts with "a2p3.net" then its a logon QR
-                    	// TODO: move a2p3.net into settings
-                    	if (result.text.indexOf("a2p3.net") == 0) {
-                    		app.mobileUrlInvokeHandler(result.text);
-                    	}
-                    	else {
-                    		// TODO: It'd be nice if both QR came in as URLs, talk to Dick
-                    		app.mobileUrlInvokeHandler("a2p3.net://enroll?code=" + result.text);
-                    	}
-                    }
-                },
-                function(error) {
-                	console.log("Scan failed callback");
-                    navigator.notification.alert("scanning failed: " + error);
-                }
-	   		)
-	        
-		},
+
 	});
 });

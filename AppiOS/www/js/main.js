2470c36140aca776abd86b0b1cbb7bf1c0d5e17d
Greg Turner <greg.fun@gmail.com>
2013-01-31 13:47:13 -0800

    turning ajax caching back to default to confirm test case.

--- AppiOS/www/js/main.js
+++ AppiOS/www/js/main.js
@@ -6,10 +6,11 @@ $(document).ready(function() {
 	console.log("jquery document ready");
 	
 	 // Disable caching of AJAX responses
+	 /*
 	$.ajaxSetup ({
 	   
 	    cache: false
-	});
+	});*/
 	
 	// Init our singltons
 	settings = new window.Agent.Settings();

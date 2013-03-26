94f728c7ccf05b38eefad421311031bd26b47451
Greg Turner <greg.fun@gmail.com>
2013-03-26 14:43:16 -0700

    fixed bad line wrap on logon

--- AppiOS/www/js/views/AgentRequestView.js
+++ AppiOS/www/js/views/AgentRequestView.js
@@ -113,7 +113,7 @@ $(function($) {
 	   			var resourceServersLoaded = this.model.get("ResourceServersLoaded");
 	   			var resourceServerTotal = this.model.get("ResourceServersTotal");
 		   		if (resourceServersLoaded < resourceServerTotal) {
-		   			this.$("#loadingBar").text("Retrieving authorization requests " + resourceServersLoaded + " of " + resourceServerTotal + "...");
+		   			this.$("#loadingBar").text("Retrieving authorization <br>requests " + resourceServersLoaded + " of " + resourceServerTotal + "...");
 		   			this.$("#loadingBar").show();
 		   			
 		   			// also hide the authorize button
